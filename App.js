34c51fdb6ac23959f1c76eb9ec1a51624d593379
anandkumarcoder <akanand104@gmail.com>
2021-09-13 01:18:45 +0530

    fixed hidding the table and error message

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ const message = document.querySelector("#error-message")
 // calculateBTN.addEventListener("click", () => console.log(cashReceived.value));
 
 const noOfNotes = document.querySelectorAll(".noOfNotes")
- const outputTable = document.querySelector(".changeReturn")
- const outputDiv = document.querySelector("#output")
+const outputTable = document.querySelector(".changeReturn")
+const outputDiv = document.querySelector("#output")
 
 
 const arrayNoteAmt = [2000, 500, 200, 100, 20, 10, 5, 1]
@@ -22,22 +22,21 @@ function hideMessage() {
 
 function showMessage(msg) {
     message.style.display = "block",
-    message.style.fontWeight = "bold"
+        message.style.fontWeight = "bold"
     message.style.color = "red"
-        message.innerText = msg;
+    message.innerText = msg;
 };
 
 // trial with next button- check on bill amount
 
 
-nextBTN.addEventListener('click' , function (){
+nextBTN.addEventListener('click', function () {
     hideMessage();
-    if (Number(billAmt.value) > 0) {
-        nextBTN.style.display = "none" ;
+    if (Number(billAmt.value) > 0 ) {
+        nextBTN.style.display = "none";
         cashReceivedDiv.style.display = "block";
-    }
-    else{
-        showMessage("Enter the valid amount")
+    } else {
+        showMessage("Bill amount must be greater than zero")
     }
 })
 
@@ -45,42 +44,50 @@ nextBTN.addEventListener('click' , function (){
 
 
 calculateBTN.addEventListener("click", function validateBillAndCashAmount() {
-   hideMessage();
-let billAmtValue = Number(billAmt.value)
-let cashReceivedValue = Number(cashReceived.value);
+    hideMessage();
+    let billAmtValue = Number(billAmt.value)
+    let cashReceivedValue = Number(cashReceived.value);
 
-if (!Number.isInteger(cashReceivedValue)){
-    showMessage("Enter the valid amount")
-}
-else if (billAmtValue > cashReceivedValue) {
- showMessage("Cash  received is less than bill amount, please enter right amount");
+   
+
+
+     if (billAmtValue === cashReceivedValue) {
+        showMessage("bill amount equal to cash received")
+        outputTable.style.display = "none";
+        outputDiv.style.display ="none";
+
     }
-else if ( cashReceivedValue > billAmtValue) {
-    const amountToBeReturned = cashReceivedValue - billAmtValue ;
-    calculateChange(amountToBeReturned);
-    outputDiv.innerHTML = "Please return Rs" + " " +(amountToBeReturned) + " " + "to the customer";
-    outputDiv.style.fontWeight ="bold"
+    else if (billAmtValue > cashReceivedValue) {
+        showMessage("Cash  received is less than bill amount, please enter right amount");
+        outputTable.style.display = "none";
+        outputDiv.style.display ="none";
+    }
 
-    outputTable.style.display = "block";
-    // outputTable.innerHTML = "Please return Rs" + " " +(amountToBeReturned) + " " + "to the customer";
+   else if (cashReceivedValue > billAmtValue) {
+        const amountToBeReturned = cashReceivedValue - billAmtValue;
+        calculateChange(amountToBeReturned);
+        outputDiv.innerHTML = "Please return Rs" + " " + (amountToBeReturned) + " " + "to the customer";
+        outputDiv.style.fontWeight = "bold"
+
+        outputTable.style.display = "block";
+    } 
     
     
-
-}  
- else {
-     showMessage("Please enter valid amount")
- } 
+    
+      else {
+        showMessage("Please enter valid amount")
+    }
 
 })
 
 // console.log(outputDiv)
-    
+
 // // calculating number of notes to be returned
 
-function calculateChange(amountToBeReturned){
-    for (let i = 0; i < arrayNoteAmt.length; i++ ) {
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < arrayNoteAmt.length; i++) {
         const numberOfNotes = Math.trunc(
-            amountToBeReturned/arrayNoteAmt[i]
+            amountToBeReturned / arrayNoteAmt[i]
         );
         // amount left after deducting 1st step
         amountToBeReturned = amountToBeReturned % arrayNoteAmt[i];
@@ -88,5 +95,3 @@ function calculateChange(amountToBeReturned){
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
-
-
